1769be3fda0e82818cda8ec884970688e1ecb000
NR221019 <143046534+al3padilla@users.noreply.github.com>
2025-04-28 21:08:07 -0600

    Delete Desafío2 directory

--- Desafío2/app/controllers/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-header("Location: /Desafío2/app/views/home");
-exit();
-?>
--- Desafío2/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config/config.php';
-require_once 'app/core/App.php';
-require_once 'app/core/Controller.php';
-require_once 'app/core/Database.php';
-require_once 'app/controllers/AdminController.php';
-
-$app = new App();
